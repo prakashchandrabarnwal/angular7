f470e61f23f1a83b4eb17c9298764c5d090eb014
prakashchandrabarnwal <barnwalbikky@gmail.com>
2019-04-21 13:37:01 +0530

    components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 
-import { HeaderClass } from './header/header.component';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
-import { RecipesListComponent } from './recipes/recipes-list/recipes-list.component';
-import { RecipesDetailsComponent } from './recipes/recipes-details/recipes-details.component';
-import { RecipeItemComponent } from './recipes/recipes-list/recipe-item/recipe-item.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShopingEditComponent } from './shopping-list/shoping-edit/shoping-edit.component'
-
- 
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderClass,
+    HeaderComponent,
     RecipesComponent,
-    RecipesListComponent,
-    RecipesDetailsComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
     RecipeItemComponent,
     ShoppingListComponent,
-    ShopingEditComponent
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     FormsModule
   ],
   providers: [],
